3c3eb98016674549ea060f3f4a8605b743ab5524
root <root@iPhone>
2022-05-17 19:24:36 -0400

    payment address 1

--- src/app/components/success/success.component.ts
+++ src/app/components/success/success.component.ts
@@ -15,7 +15,7 @@ export class SuccessComponent implements OnInit {
 
   ngOnInit(): void {
     if(this._butler.metodo==1){
-      this.paymentAddress="";
+      this.paymentAddress="0x0b55a324194e731bb8f19fd007dbe5af8fa17792";
       this.mensaje="Su pedido ha sido registrado exitosamente,  por favor realice el pago en USDT a la siguiente direccion wallet:"
     }if(this._butler.metodo==2){
       this.paymentAddress="";
